b00350515e7768c9e22f5b7640a1bf0a354ae698
Steven Bell <steven.bell@sunlife.com>
2019-05-23 17:07:11 -0400

    Refactored IData merge logic into a utility class. Changed PipelineVariableStep to evaluate JEXL expression on new IData each time, then merge into context IData to avoid "Given pipeline values doc.field1.string1="val"; doc.field2.string2="val"" bug.

--- src/main/java/org/wmaop/bdd/steps/InvokeServiceStep.java
+++ src/main/java/org/wmaop/bdd/steps/InvokeServiceStep.java
@@ -1,9 +1,9 @@
 package org.wmaop.bdd.steps;
 
+import org.wmaop.bdd.utils.IDataMergeTool;
+
 import com.wm.data.IData;
-import com.wm.data.IDataCursor;
 import com.wm.data.IDataFactory;
-import com.wm.data.IDataUtil;
 
 public class InvokeServiceStep extends BaseServiceStep {
 
@@ -25,61 +25,10 @@ public class InvokeServiceStep extends BaseServiceStep {
 		}
 		IData override = executionContext.getPipeline();
 		if (override != null) {
-			mergeNestedTypes(override, inputPipeline);
+			IDataMergeTool.mergeNestedTypes(override, inputPipeline);
 		}
 		IData pipeline = invokeService(executionContext, serviceName, inputPipeline);
 		executionContext.setPipeline(pipeline);
 	}
 	
-	private static void mergeNestedTypes(IData override, IData inputPipeline){
-		IDataCursor orCursor = override.getCursor();
-		IDataCursor ipCursor = inputPipeline.getCursor();
-		
-		// Loop over override idata keys
-		String currentKey = null;
-		while(orCursor.hasMoreData()){
-			orCursor.next();
-			currentKey = orCursor.getKey();
-			
-			// Check if the overwrite value is an IData or IDataArray
-			if(IDataUtil.getIDataArray(orCursor, currentKey) != null
-					&& IDataUtil.getIDataArray(ipCursor, currentKey) != null){
-				//If we have an IDataArray in the overwrite pipeline
-				//and existing IDataArray in the input pipeline
-				//Call a recursive merge of these two IDataArrays
-				IData[] mergedArray = mergeNestedTypes(IDataUtil.getIDataArray(orCursor, currentKey), IDataUtil.getIDataArray(ipCursor,currentKey));
-				IDataUtil.put(ipCursor, currentKey, mergedArray);
-			}
-			else if(IDataUtil.getIData(orCursor, currentKey) != null
-					&& IDataUtil.getIData(ipCursor, currentKey) != null){
-				//If we have an IData in the overwrite pipeline
-				//and existing IData in the input pipeline
-				//Call a recursive merge of these two IDatas
-				mergeNestedTypes(IDataUtil.getIData(orCursor, currentKey), IDataUtil.getIData(ipCursor,currentKey));
-				//Don't need to put the IData because the reference was already merged
-			}
-			else{
-				//Copy the value to the inputPipeline
-				IDataUtil.put(ipCursor, currentKey, orCursor.getValue());
-			}				
-		}
-	}
-	
-	private static IData[] mergeNestedTypes(IData[] overrideArray, IData[] inputPipelineArray){
-		//Find the larger size of the two input arrays
-		//And create the result array of this size
-		int largerSize = Math.max(overrideArray.length, inputPipelineArray.length);
-		IData[] result = new IData[largerSize];
-		
-		//Loop through inputPipelineArray and copy over to result array
-		for(int i=0; i<inputPipelineArray.length; i++){
-			result[i] = inputPipelineArray[i];
-		}
-		//Loop over each overrideArray element
-		//and call to merge the IData
-		for(int i=0; i<overrideArray.length;i++){
-			mergeNestedTypes(overrideArray[i],result[i]);
-		}
-		return result;
-	}
 }
